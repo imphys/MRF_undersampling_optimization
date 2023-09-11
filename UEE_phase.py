4a7c0eddb8be2a6ce8a36c97a01fafcf13e7665e
Martijn Nagtegaal <50950962+MNagtegaal@users.noreply.github.com>
2023-09-11 10:20:35 +0200

    bug fix copy statement

--- UEE_phase.py
+++ UEE_phase.py
@@ -687,7 +687,7 @@ class JAC():
         self.step = step
 
     def jac_step(self, ii):
-        Opt_one_step_forward = self.Opt_param
+        Opt_one_step_forward = np.copy(self.Opt_param)
         Opt_one_step_forward[ii] = self.Opt_param[ii] + self.step[ii]
         one_step_forward = self.Prob.full_run(Opt_one_step_forward)
 
